3e219775fd96956a39bbd8dbed34fa3a9a99f85f
Giovanni Lucca Reque Uchoa <gi.lucca10@gmail.com>
2023-08-24 21:35:50 -0300

    make it objects oriented
    giovanni uchoa

--- flood fill/src/Main.java
+++ flood fill/src/Main.java
@@ -1,52 +1,13 @@
+// Main.java
 public class Main {
     public static void main(String[] args) {
+        Matriz matriz = new Matriz(10, 10);
 
-        // Cria a matriz
-        int[][] matriz = new int[10][10];
+        matriz.preencherZerosNaDiagonal();
+        matriz.preencherUnsNoRestante();
 
-        // Coloca os 0 na diagonal
-        for (int i = 0; i < 10; i++) {
-            matriz[i][9 - i] = 0;
-        }
+        matriz.imprimirMatriz();
 
-        // Coloca os 1 no resto
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (i != 9 - j) {
-                    matriz[i][j] = 1;
-                }
-            }
-        }
-
-        // Imprime a matriz
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                System.out.print(matriz[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println("\n");
-
-        // Verifica os números
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (matriz[i][j] == 1) {
-                    matriz [i][j] = 2;
-                } else if (matriz[i][j] == 0 ) {
-                    break;
-                }
-
-            }
-
-            // Imprime a matriz
-            for (int a = 0; a < 10; a++) {
-                for (int b = 0; b < 10; b++) {
-                    System.out.print(matriz[a][b] + " ");
-                }
-                System.out.println();
-            }
-            System.out.println("\n");
-
-        }
+        matriz.verificarEAtualizarNumeros();
     }
 }
---
+++ flood fill/src/Matriz.java
@@ -0,0 +1,47 @@
+// Matriz.java
+public class Matriz {
+    private int[][] matriz;
+
+    public Matriz(int linhas, int colunas) {
+        matriz = new int[linhas][colunas];
+    }
+
+    public void preencherZerosNaDiagonal() {
+        for (int i = 0; i < matriz.length; i++) {
+            matriz[i][matriz.length - 1 - i] = 0;
+        }
+    }
+
+    public void preencherUnsNoRestante() {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                if (i != matriz.length - 1 - j) {
+                    matriz[i][j] = 1;
+                }
+            }
+        }
+    }
+
+    public void imprimirMatriz() {
+        for (int[] linha : matriz) {
+            for (int valor : linha) {
+                System.out.print(valor + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public void verificarEAtualizarNumeros() {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                if (matriz[i][j] == 1) {
+                    matriz[i][j] = 2;
+                } else if (matriz[i][j] == 0) {
+                    break;
+                }
+            }
+            imprimirMatriz();
+        }
+    }
+}
