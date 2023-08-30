a12ced8088d80f06874438057d90beb659d9e267
GuilhermeTEREZIN <gui158gta@hotmail.com>
2023-08-30 18:30:57 -0300

    new fila
    gui

--- flood fill/src/Fila.java
+++ flood fill/src/Fila.java
@@ -2,26 +2,71 @@ public class Fila <T>{
     private T[] data;
     private int topo;
     private int base;
-    private int tamanho;
+    private int tamanhoAtual;
 
-    public Fila(int tamanho){
+    public Fila(int capacidadeTotal){
         topo = -1;
         base = 0;
-        this.tamanho = tamanho;
-        this.data = (T[]) new Object[tamanho];
+        this.tamanhoAtual = 0;
+        this.data = (T[]) new Object[capacidadeTotal];
 
     }
-    public void add(T data);
-    public T remove();
-    public boolean isEmpty();
-    public boolean isFull();
+    public void add(T data){
+        if (isFull()){
+            throw new IllegalStateException("A fila está cheia.");
+        }
+
+        setTopo((getTopo()+1)% getData().length);
+
+        this.data[getTopo()] = data;
+        setTamanhoAtual(tamanhoAtual + 1);
+    }
+    public T remove(){
+        if (isEmpty()){
+            throw new IllegalStateException("A fila está vazia.");
+        }
+        T retorno = this.data[getBase()];
+        this.data[getBase()] = null;
+
+        setBase(base + 1);
+        setTamanhoAtual(tamanhoAtual - 1);
+        return retorno;
+    }
+    public boolean isEmpty(){
+        if (getTamanhoAtual() == 0){
+            return true;
+        }
+        return false;
+    }
+    public boolean isFull(){
+        if(getTamanhoAtual() == data.length){
+            return true;
+        }
+        return false;
+    }
 
     public void clear(){
         for (int i = 0; i<data.length;i++){
             this.data[i] = null;
         }
-        topo = -1;
+        setTopo(-1);
     }
+    public void showFila(){
+
+        System.out.println("Topo: "+getTopo()+" Base: "+getBase());
+        for (int i = 0; i<data.length;i++){
+            if ( data[i] != null){
+                System.out.println(i + " - "+data[i].toString());
+            }else {
+                System.out.println(i+" - Null");
+            }
+        }
+    }
+
+    public int getTamanhoAtual() {
+        return tamanhoAtual;
+    }
+
     public void setBase(int base) {
         this.base = base;
     }
@@ -34,8 +79,8 @@ public class Fila <T>{
         this.topo = topo;
     }
 
-    public void setTamanho(int tamanho) {
-        this.tamanho = tamanho;
+    public void setTamanhoAtual(int tamanho) {
+        this.tamanhoAtual = tamanho;
     }
 
     public int getBase() {
@@ -47,7 +92,7 @@ public class Fila <T>{
     }
 
     public int getTamanho() {
-        return tamanho;
+        return tamanhoAtual;
     }
 
     public T[] getData() {
